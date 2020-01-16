50d2edd2deeee6033f6aa2618a8aa1d43a955c8b
Samuel Pozzobon <42522736+SamuelPozzobon@users.noreply.github.com>
2020-01-16 19:16:52 +0100

    FIXATO ES. 53

---
+++ es. 53 con textbox e label/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim pezzi, prezzo, sconto, tot1, totale As Double
+        prezzo = Val(TextBox1.Text)
+        pezzi = Val(TextBox2.Text)
+        tot1 = prezzo * pezzi
+        If (pezzi = 1) Then
+            sconto = (tot1 * 10) / 100
+            totale = tot1 - sconto
+        ElseIf (pezzi = 3) Then
+            sconto = (tot1 * 20) / 100
+            totale = tot1 - sconto
+        ElseIf (pezzi = 5) Then
+            sconto = (tot1 * 30) / 100
+            totale = tot1 - sconto
+        ElseIf (pezzi = 10) Then
+            sconto = (tot1 * 35) / 100
+            totale = tot1 - sconto
+        ElseIf (pezzi > 10) Then
+            sconto = (tot1 * 40) / 100
+            totale = tot1 - sconto
+        Else
+            totale = tot1
+            TextBox3.Text = tot1
+        End If
+        TextBox3.Text = totale
+    End Sub
+End Class
---
+++ es. 53 con textbox e label/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Le informazioni generali relative a un assembly sono controllate dal seguente 
+' set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+' associate a un assembly.
+
+' Controllare i valori degli attributi degli assembly
+
+<Assembly: AssemblyTitle("es. 53 con textbox e label")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("es. 53 con textbox e label")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
+<Assembly: Guid("8274c30a-db7e-4262-9198-00e64d27f960")>
+
+' Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+'
+'      Versione principale
+'      Versione secondaria
+'      Numero di build
+'      Revisione
+'
+' È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
+' usando l'asterisco '*' come illustrato di seguito:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
